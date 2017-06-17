fa79aeaeae1bd935fb5b8c10c358982047a6b85c
mytac <my_tac@126.com>
2017-06-17 15:52:55 +0800

    整理格式

--- spy.js
+++ spy.js
@@ -1,3 +1,18 @@
+const mainStyle=(ele,size,color)=>{
+    $(ele).css({
+        width: size+'px',
+        height:size+'px',
+        borderRadius: '50%',
+        backgroundColor: color,
+        position: 'absolute',
+        margin: 'auto',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+    })
+}
+
 class spy {
     constructor(obj) {
         const {color,eyeSize,element,containerEl}=obj
@@ -8,59 +23,46 @@ class spy {
 
         this.init()
     }
+    // 绑定事件
     bindEvent(){
-
-    }
-    render(){
-
-    }
-    mainStyle(ele,size,color){
-        $(ele).css({
-            width: size+'px',
-            height:size+'px',
-            borderRadius: '50%',
-            backgroundColor: color,
-                position: 'absolute',
-            margin: 'auto',
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-        })
-    }
-    // 根据class生成几个子元素
-    generateNewElements(){
         const options={
             relativeInput:false,
             invertX:false,
             invertY:false,
         }
-        // 设置每层的class名
-        const wrapperC=this.element.substring(1)
-        const classNames=[wrapperC+'0',wrapperC+'1',wrapperC+'2']
-        const elements=classNames.map((c,index)=>`<div class='layer ${c}'  data-depth=${this.ratio[index]}></div>`)
-        $(this.element).append(elements)
         // 给container挂上parallax
         const scene1 = $(this.containerEl).get(0)
         const parallax1 = new Parallax(scene1,options)
+    }
+    //处理样式
+    handleStyle(){
         // 最外层样式
         $(this.containerEl).css({position:'absolute'})
         $(this.element).css({position:'relative'})
         //每一层的样式
         this.eyeSizeGroup.forEach((size,index)=>{
-            this.mainStyle(`.${classNames[index]}`,size,this.color[index])
+            mainStyle(`.${this.classNames[index]}`,size,this.color[index])
         })
-        console.log('classNames',classNames)
+    }
+    // 根据class生成几个子元素
+    generateNewElements(){
+        // 设置每层的class名
+        const wrapperC=this.element.substring(1)
+        this.classNames=[wrapperC+'0',wrapperC+'1',wrapperC+'2']
+        const elements=this.classNames.map((c,index)=>`<div class='layer ${c}'  data-depth=${this.ratio[index]}></div>`)
+        $(this.element).append(elements)
     }
 
-    eyeStyle(){
+    eyeStyleOption(){
         const mainSize=this.eyeSize
         this.eyeSizeGroup=[mainSize,0.4*mainSize,0.15*mainSize]
         this.ratio=[0,2.5,3]
     }
 
     init(){
-        this.eyeStyle()
+        this.eyeStyleOption()
         this.generateNewElements()
+        this.bindEvent()
+        this.handleStyle()
     }
 }
