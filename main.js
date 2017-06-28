ca4ec82b8c901557b3ec2399422b6021f11f8319
张君卓 <my_tac@126.com>
2017-06-28 18:15:03 +0800

    basic compeleted

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 import Spy from './spy2'
 render(
-    <div>
-       <Spy outer="outer" inner="inner"></Spy>
-    </div>,
+       <Spy outer="outer" inner="inner"></Spy>,
     document.querySelector('#root')
 )
--- spy2.js
+++ spy2.js
@@ -18,9 +18,18 @@ const Eyeball = (props) => {
     const {color, size, inner} = props
     const sizeGroup = [size, 0.5 * size, 0.32 * size]
     const ratio = [0, size * 0.0045, size * 0.005]
+    const wrapperStyle={
+        position: 'relative',
+        margin: 'auto',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+    }
     return (
-        <div>
-            {color.map((c, index) => <div className={`layer ${inner}-${index}`} key={c} data-depth={ratio[index]}
+        <div className={inner} style={wrapperStyle}>
+            {color.map((c, index) => <div className={`layer ${inner}-${index}`} key={c}
+                                          data-depth={ratio[index]}
                                           style={handleStyle(sizeGroup[index], color[index])}></div>
             )}
         </div>
@@ -30,12 +39,13 @@ const Eyeball = (props) => {
 export default class Spy extends React.Component {
     constructor(props) {
         super(props)
-        const {color, size, dopeMove, outer, inner} = props
+        const {color, size, dopeMove, outer, inner,position} = props
         this.color = color || ['#f5f0d8', 'rgb(24, 198 , 184)', 'black']
         this.size = size || 250
         this.dopeMove = dopeMove || false
         this.outer = outer || 'outer'
         this.inner = inner || 'inner'
+        this.position=position||{top:'50%',left:'50%'}
     }
 
     componentDidMount() {
@@ -45,12 +55,23 @@ export default class Spy extends React.Component {
             invertX: false,
             invertY: false,
         }
-
+        const scene=document.getElementById(this.outer)
+        new Parallax(scene, options)
     }
 
     render() {
+        const outerStyle=Object.assign(
+            {
+                width:this.size,
+                height:this.size
+            },
+            this.position
+        )
+        console.log(outerStyle)
         return (
-            <Eyeball size={this.size} color={this.color} inner={this.inner}/>
+            <div id={this.outer} style={outerStyle}>
+                <Eyeball size={this.size} color={this.color} inner={this.inner}/>
+            </div>
         )
     }
 }
