e6a85b7a4b615ecf46f4150921a561268cf507ee
张君卓 <my_tac@126.com>
2017-06-28 17:17:05 +0800

    eyeball construction

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import {Spy} from 'spy2'
+import Spy from './spy2'
 render(
     <div>
-        <Spy></Spy>
+       <Spy outer="outer" inner="inner"></Spy>
     </div>,
     document.querySelector('#root')
 )
--- spy2.js
+++ spy2.js
@@ -1,22 +1,56 @@
 import React from 'react';
-import Parallax from 'parallax-js'
-import PropTypes from 'prop-types';
+import Parallax from './static/parallax.js'
+// eyeball color
+const handleStyle = (size, color) => ({
+    width: size + 'px',
+    height: size + 'px',
+    borderRadius: '50%',
+    backgroundColor: color,
+    position: 'absolute',
+    margin: 'auto',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+})
+
+const Eyeball = (props) => {
+    const {color, size, inner} = props
+    const sizeGroup = [size, 0.5 * size, 0.32 * size]
+    const ratio = [0, size * 0.0045, size * 0.005]
+    return (
+        <div>
+            {color.map((c, index) => <div className={`layer ${inner}-${index}`} key={c} data-depth={ratio[index]}
+                                          style={handleStyle(sizeGroup[index], color[index])}></div>
+            )}
+        </div>
+    )
+}
+
 export default class Spy extends React.Component {
-    constructor(props){
-        const {color, eyeSize, dopeMove}=obj
-        this.color = color || ['#f5f0d8', 'rgb(24, 198, 184)', 'black']
-        this.eyeSize = eyeSize || 250
+    constructor(props) {
+        super(props)
+        const {color, size, dopeMove, outer, inner} = props
+        this.color = color || ['#f5f0d8', 'rgb(24, 198 , 184)', 'black']
+        this.size = size || 250
         this.dopeMove = dopeMove || false
-        this.init()
+        this.outer = outer || 'outer'
+        this.inner = inner || 'inner'
     }
-    componentDidMount(){
-        console.log(document.getElementsByClassName('eyeball'))
+
+    componentDidMount() {
+        // 绑定Parallax
+        const options = {
+            relativeInput: false,
+            invertX: false,
+            invertY: false,
+        }
+
     }
-    render(){
-        return(
-            <div className="eyeball">
-                ssss
-            </div>
+
+    render() {
+        return (
+            <Eyeball size={this.size} color={this.color} inner={this.inner}/>
         )
     }
 }
